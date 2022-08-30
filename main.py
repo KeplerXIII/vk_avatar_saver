997ba273229bb88d8b7ebde595f02bcd256b981f
Alexandr Pushkin (work) <a.v.pushkin@gmail.com>
2022-08-30 11:07:39 +0300

    update repo

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
-import user
-from pprint import pprint
-import yadisk
-from vk_api import get_photo_dict
 from datetime import datetime
+from pprint import pprint
+import user
+import yadisk
 import operator
+import vk_api
 
+VK_TOKEN = user._vk_token
+YA_TOKEN = user._ya_token
+VK_ID = user._vk_self_id
 
-ya = yadisk.YandexDisk(token=user._ya_token)
-dict = get_photo_dict(user._vk_self_id)
-
-def synchronization_vk_ya():
+def vk_yadisk_parser(VK_TOKEN, YA_TOKEN, VK_ID, quantity=5):
     success = 0
+    result = []
+    vk = vk_api.Vkontakte(token=VK_TOKEN)
+    ya = yadisk.YandexDisk(token=YA_TOKEN)
+    dict = vk.get_photo_dict(VK_ID)
+    counter = len(dict['response']['items'])
+    print(f'Найдено {counter} фотографий')
     for item in dict['response']['items']:
-        counter = len(dict['response']['items'])
-        for size in item['sizes']:
-            if size['type'] == 'w' or size['type'] == 'z':
-                good_look_date = datetime.fromtimestamp(item["date"]).strftime("%Y, %d %B")
-                ya.upload_url_to_disk(f'test/{good_look_date}- {item["likes"]["count"]} likes.jpg', size['url'])
-                success += 1
-                print(f'Записано {success} из {counter}')
+        good_look_date = datetime.fromtimestamp(item["date"]).strftime("%Y, %d %B")
+        item['sizes'].sort(key=operator.itemgetter('height'))
+        ya.upload_url_to_disk(f'test/{good_look_date} - {item["likes"]["count"]} likes.jpg', item['sizes'][-1]['url'])
+        result.append({'file_name': f'{good_look_date} - {item["likes"]["count"]} likes.jpg', 'size': f'{item["sizes"][-1]["type"]}'})
+        success += 1
+        print(f'Сохранено {success} из {counter}')
+        if success >= quantity:
+            break
 
+    pprint(result)
 
-
-for item in dict['response']['items']:
-    # pprint(item['sizes'])
-    item['sizes'].sort(key=operator.itemgetter('height'))
-    print(item['sizes'][-1])
+vk_yadisk_parser(VK_TOKEN, YA_TOKEN, VK_ID)
 
 
 
--- vk_api.py
+++ vk_api.py
@@ -1,17 +1,22 @@
 import user
 import requests
 
-def get_photo_dict(vk_id):
-    URL = 'https://api.vk.com/method/photos.get'
-    params = {
-        'owner_id': vk_id,
-        'access_token': user._vk_token,
-        'album_id': "profile",
-        'photo_sizes': '1',
-        'extended': '1',
-        'v': '5.131'
-    }
-    res = requests.get(URL, params=params)
-    res = res.json()
 
-    return res
+class Vkontakte:
+    def __init__(self, token):
+        self.token = token
+
+    def get_photo_dict(self, vk_id):
+        URL = 'https://api.vk.com/method/photos.get'
+        params = {
+            'owner_id': vk_id,
+            'access_token': user._vk_token,
+            'album_id': "profile",
+            'photo_sizes': '1',
+            'extended': '1',
+            'v': '5.131'
+        }
+        res = requests.get(URL, params=params)
+        res = res.json()
+
+        return res
